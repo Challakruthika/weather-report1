eb232aad330e6fa2c8c80e37b745e4bfb754300b
Challakruthika <156560243+Challakruthika@users.noreply.github.com>
2025-05-12 06:54:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-/** @type {import('vite').UserConfig} */
-const config = {
-  plugins: [sveltekit()],
-  preview: {
-    port: 3000,
-    host: true,
-    allowedHosts: ['weather-tracker-9dfv.onrender.com']
-  }
-};
+export default defineConfig({
+  plugins: [sveltekit()]
+});
